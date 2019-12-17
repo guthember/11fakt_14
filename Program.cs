8906422f0d14000f30cd9617e52b1adc1a40c61c
Erik Gúth <guthember@gmail.com>
2019-12-17 15:52:13 +0100

    Megoldott

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace atvalto
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.Write("Kérek egy egész számot: ");
+      int szam = Convert.ToInt32(Console.ReadLine());
+
+      Console.WriteLine("Kérek egy számrendszert (9-ig): ");
+      int rendszer = Convert.ToInt32(Console.ReadLine());
+
+      string binaris = "";
+
+      while (szam != 0)
+      {
+        int maradek = szam % rendszer;
+        binaris = maradek.ToString() + binaris;
+        szam = szam / rendszer;
+      }
+
+      Console.WriteLine(binaris);
+      Console.ReadKey();
+    }
+  }
+}
